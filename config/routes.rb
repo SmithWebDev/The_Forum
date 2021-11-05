2e5cfa9788f4b5e1bac9e5740d216a9664af817b
Darrick Smith <smithwebdd@gmail.com>
2021-11-04 21:07:10 -0400

    Edit Discussions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :discussions, only: %i[index create new]
+  resources :discussions, only: %i[index create new edit update]
   root to: 'main#index'
 end
