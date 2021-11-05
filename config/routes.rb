945214f73ab5104d52b4201d9e82c078b81e1bda
Darrick Smith <smithwebdd@gmail.com>
2021-11-04 21:44:10 -0400

    Delete Discussions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :discussions, only: %i[index create new edit update]
+  resources :discussions, only: %i[index create new edit update destroy]
   root to: 'main#index'
 end
